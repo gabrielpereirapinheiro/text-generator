34a6f1a4977c55fa791e83a7398de19b2e9e475e
gabrielpereirapinheiro <gabriel.pereira.pinheiro@gmail.com>
2024-02-24 16:34:25 -0300

    'test'

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import logo from "./doctor.png";
 import "./App.css";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { useState } from "react";
+import Steps from "./steps";
 
 function App() {
+  const [start, setStart] = useState(false);
+
   return (
     <ChakraProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      {!start ? (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Button
+              onClick={() => setStart(true)}
+              size="md"
+              height="48px"
+              width="200px"
+              border="2px"
+              borderColor="green.500"
+            >
+              Iniciar
+            </Button>
+          </header>
+        </div>
+      ) : (
+        <Steps />
+      )}
     </ChakraProvider>
   );
 }
--- src/steps.js
+++ src/steps.js
@@ -2,15 +2,50 @@ import logo from "./doctor.png";
 import "./App.css";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Tabs, Box, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { useState } from "react";
+import { steps, screens } from "./setup/steps";
+
+
+const findScreen = (id) => {
+    let aux = screens.filter((id)=>)
+}
 
 function Steps() {
-  // 2. Wrap ChakraProvider at the root of your app
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleSliderChange = (event) => {
+    setTabIndex(parseInt(event.target.value, 10));
+  };
+
+  const handleTabsChange = (index) => {
+    setTabIndex(index);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button>Iniciar</button>
-      </header>
+    <div className="Steps">
+      <Box>
+        <input
+          type="range"
+          min="0"
+          max="2"
+          value={tabIndex}
+          onChange={handleSliderChange}
+        />
+
+        <Tabs index={tabIndex} onChange={handleTabsChange}>
+          <TabList>
+            {steps.map((item) => (
+              <Tab>{item}</Tab>
+            ))}
+          </TabList>
+          <TabPanels>
+            {steps.map((item) => (
+              <TabPanel>teste{item}</TabPanel>
+            ))}
+          </TabPanels>
+        </Tabs>
+      </Box>
     </div>
   );
 }
