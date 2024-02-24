c2fb8227ebfdbcf8a4bb7fa7fcece34b3c7841f7
gabrielpereirapinheiro <gabriel.pereira.pinheiro@gmail.com>
2024-02-24 17:17:27 -0300

    'first'

--- src/setup/steps.js
+++ src/setup/steps.js
@@ -1,23 +1,35 @@
-const steps = [1, 2, 3];
+const steps = [1, 2, 3, 4];
 
 const screens = [
   {
     title: "Nome",
     id: 1,
+    placeholder: "Digite o nome do paciente",
     type: "input",
     text: "O paciente <replace>",
   },
   {
     title: "Temperatura",
     id: 2,
+    placeholder: "Digite a temperatura",
     type: "input",
     text: "estava com temperatura corporal <replace> graus celsius",
   },
   {
-    title: "Pressao estavel",
+    title: "Pressao",
     id: 3,
     type: "options",
+    options: [
+      { label: "Alta", value: "Alta" },
+      { label: "Normal", value: "Normal" },
+      { label: "Baixa", value: "Baixa" },
+    ],
     text: "estava com pressao <replace>",
+  },
+  {
+    title: "End",
+    id: 4,
+    type: "generator",
   },
 ];
 
--- src/steps.js
+++ src/steps.js
@@ -5,14 +5,89 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Tabs, Box, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import { useState } from "react";
 import { steps, screens } from "./setup/steps";
+import { Input } from "@chakra-ui/react";
+import { Radio, RadioGroup } from "@chakra-ui/react";
+import { Heading, Stack } from "@chakra-ui/react";
 
+const updateInput = (e, info, value, setValue) => {
+  let aux = value;
+  aux[info.id - 1] = e.target.value;
+  setValue(aux);
+};
 
-const findScreen = (id) => {
-    let aux = screens.filter((id)=>)
-}
+const inputQuestion = (info, value, setValue) => {
+  return (
+    <>
+      <Heading>{info.title}</Heading>
+      <Input
+        onChange={(e) => updateInput(e, info, value, setValue)}
+        placeholder={info.placeholder}
+      />
+      ;
+    </>
+  );
+};
+
+const updateOption = (e, info, value, setValue) => {
+  let aux = value;
+  aux[info.id - 1] = e.target.value;
+  setValue(aux);
+};
+
+const optionsQuestion = (info, value, setValue) => {
+  return (
+    <>
+      <Heading>{info.title}</Heading>
+
+      <RadioGroup>
+        <Stack direction="row">
+          {info.options.map((item) => {
+            return (
+              <Radio
+                onChange={(e) => updateOption(e, info, value, setValue)}
+                value={item.value}
+              >
+                {item.label}
+              </Radio>
+            );
+          })}
+        </Stack>
+      </RadioGroup>
+    </>
+  );
+};
+
+const textGenerator = (info) => {
+  return (
+    <>
+      <Heading>{info.title}</Heading>
+    </>
+  );
+};
+
+const findScreen = (id, value, setValue) => {
+  let screen = screens.filter((item) => {
+    return item.id === id;
+  });
+
+  switch (screen[0].type) {
+    case "input":
+      return inputQuestion(screen[0], value, setValue);
+    case "options":
+      return optionsQuestion(screen[0], value, setValue);
+    case "generator":
+      return textGenerator(screen[0]);
+    default:
+      return "erro";
+  }
+};
 
 function Steps() {
   const [tabIndex, setTabIndex] = useState(0);
+
+  let inital = steps.map((item) => []);
+
+  const [stepValue, setStepValue] = useState(inital);
 
   const handleSliderChange = (event) => {
     setTabIndex(parseInt(event.target.value, 10));
@@ -41,7 +116,7 @@ function Steps() {
           </TabList>
           <TabPanels>
             {steps.map((item) => (
-              <TabPanel>teste{item}</TabPanel>
+              <TabPanel>{findScreen(item, stepValue, setStepValue)}</TabPanel>
             ))}
           </TabPanels>
         </Tabs>
